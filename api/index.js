5bf53e7ab2a72261a5eabc59d5a3324b42770f82
Tharun R <tharunr1705@gmail.com>
2026-03-03 20:50:18 +0530

    Vercel Deployment Fix Try 1

--- api/index.js
+++ api/index.js
@@ -1,7 +1,6 @@
 // Vercel Serverless Function Entry Point for Express Backend
 // This wraps the entire Express app as a single serverless function
 
-const app = require('../backend/src/app');
+import app from '../backend/src/app.js';
 
-// Export the Express app as a serverless function handler
-module.exports = app;
+export default app;
